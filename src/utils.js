4a2882e9281c840768a7611fb4698d770ccea292
Goga <gogaimerlishvili1@gmail.com>
2022-02-09 18:12:26 +0400

    commit

---
+++ src/utils.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const paginate = (followers) => {
+  const itemsPerPage = 10
+  const pages =Math.ceil(followers.length / itemsPerPage)
+
+  const newFollowers = Array.from({length:pages},(_,index) => {
+      const start = index * itemsPerPage
+    return followers.slice(start,start + itemsPerPage)
+    //   console.log(index)
+  })
+  return newFollowers
+}
+
+export default paginate
